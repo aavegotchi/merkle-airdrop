1c42f2d04325a73d46e0779b4fc8f21da1145b69
Coderdan <coderdannn@gmail.com>
2021-06-26 23:16:53 +0800

    added packages

--- truffle-config.js
+++
@@ -1,9 +0,0 @@
-require('babel-register');//({
-//  ignore: /node_modules\/(?!zeppelin-solidity\/test\/helpers)/
-//});
-require('babel-polyfill');
-
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
