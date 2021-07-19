f237e74d4144403a0ba9f4b85796087a84b5e368
Timidan <akinnusotutemitayodaniel@gmail.com>
2021-07-19 04:45:12 +0000

    add Birthday ticket drop script

--- scripts/addAirdrops.js
+++ scripts/addAirdrops.js
@@ -6,31 +6,33 @@
 async function AddAirdrops() {
   const AddressDropRoot =
     "0xe81b116d1a2dc5d823dcaee0b9fabf195dc1ac51680038003c60bf66984115c9";
-  const GotchiDropRoots =
-    "0x5c21f52c93fd5b0dd56e8a3a4ef57f42afae71dad9b03b34ac51d0c345d80b0a";
-  const addressItems = [0, 1, 2, 3, 4, 5, 6];
-  const gotchiItems = [1, 3];
+
+  // const GotchiDropRoots =
+  //  "0x5c21f52c93fd5b0dd56e8a3a4ef57f42afae71dad9b03b34ac51d0c345d80b0a";
+  const addressItems = [0, 1, 2, 3, 4, 6];
+  //const gotchiItems = [1, 3];
   const accounts = await ethers.getSigners();
+
   const contractOwner = accounts[0];
-  const diamondAddress = "0xA4fF399Aa1BB21aBdd3FC689f46CCE0729d58DEd";
+  const diamondAddress = "0x86935F11C86623deC8a25696E1C19a8659CbF95d"; //polygon mainnet
   console.log("Adding with the account", contractOwner.address);
 
   //deploy the merkleDiamond
   //const airdropDiamond = await deployDiamond();
   const dropContract = await ethers.getContractAt(
     "MerkleAirdropFacet",
-    "0x886ABAAEb9De0eE18A163944A6A468158502a11F"
+    "Merkle airdrop Diamond address here"
   );
 
   //add first address airdrop
   await dropContract.addAddressAirdrop(
-    "Kovan Address Test",
-    AddressDropRoot,
+    "Birthday Party Tickets", //airdrop name
+    AddressDropRoot, //merkle root
     diamondAddress,
-    1128,
-    addressItems
+    1128, //total no of people
+    addressItems //items to be claimed
   );
-
+  /** 
   //add first gotchi airdrop
   await dropContract.addGotchiAirdrop(
     "Kovan Gotchi Test",
@@ -67,8 +69,9 @@ async function claim() {
   const tx = await dropContract.claimForAddress(0, 2, 1, proof, "0x00");
   console.log("claiming in:", tx.wait());
 }
-
-claim()
+*/
+}
+AddAirdrops()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
