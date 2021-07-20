857ca5ca9db7c07f8c7834574d0515c1cd581824
Coderdan <coderdannn@gmail.com>
2021-07-20 15:13:01 +0800

    add mit license

--- contracts/facets/MerkleAirdropFacet.sol
+++ contracts/facets/MerkleAirdropFacet.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.1;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
--- contracts/interfaces/IMerkleDistributor.sol
+++ contracts/interfaces/IMerkleDistributor.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.1;
 
 // Allows anyone/any gotchi to claim a token if they exist in a merkle root.
